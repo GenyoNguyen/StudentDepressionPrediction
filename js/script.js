433a5b4dcbcaa7b2367f8147a352eb0b87b85d75
GenyoNguyen <huydragon234@gmail.com>
2025-05-19 10:39:20 +0700

    update backend endpoint

--- js/script.js
+++ js/script.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", () => {
         const data = Object.fromEntries(formData.entries());
 
         try {
-            const response = await fetch("http://localhost:8000/predict", {
+            const response = await fetch("https://f804-171-235-163-49.ngrok-free.app/predict", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
